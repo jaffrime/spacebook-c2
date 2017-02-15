52c36e3696a747224b7050cccbd7e16b3fcd744b
Julian Affrime <julian.affrime@gmail.com>
2017-02-15 13:39:44 +0200

    extension 1

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var idCounter = 0;
 var addPosts = function() {
   $('.posts').empty();
   for (i=0; i<posts.length; i++){
-    $('.posts').append("<p class='post' data-id=" + posts[i].id + ">" + posts[i].text + "</p>");
+    $('.posts').append("<p class='post' data-id=" + posts[i].id + "><a href='#' class='remove'>remove</a> " + posts[i].text + "</p>");
   }
 };
 
@@ -21,4 +21,27 @@ var newPost = function(text) {
 
 $('.add-post').on('click', function(){
   newPost($('#post-name').val());
+});
+
+$('.posts').on('click', '.remove', function(){
+  // // remove html
+  // $(this).closest('p').remove();
+  // // remove array item
+  // var arrayId = $(this).closest('p').data().id;
+  // //console.log(arrayId); // verifying data-id
+  // for (i=0; i<posts.length; i++) {
+  //   if (posts[i].id === arrayId) {
+  //     posts.splice(i,1);
+  //   }
+  // }
+  // //console.log(posts); // checking array
+
+  // using variable to capture element
+  var locLine = $(this).closest('p');
+  locLine.remove();
+  for (i=0; i<posts.length; i++) {
+    if (posts[i].id === locLine.data().id) {
+      posts.splice(i,1);
+    }
+  }
 });
